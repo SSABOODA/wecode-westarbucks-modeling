fd7d036caa70bfd669482c1ecd9d4027e08be7de
SSABOODA <69753846+SSABOODA@users.noreply.github.com>
2021-05-24 15:17:54 +0900

    error modify

--- products/migrations/0006_allergyproducts.py
+++ products/migrations/0006_allergyproducts.py
@@ -14,12 +14,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AllergyProducts',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('allergy', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.allergy')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.product')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('allergy', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='products.allergy')),
+                ('product', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='products.product')),
             ],
             options={
                 'db_table': 'allergy_products',
             },
-        ),
+        )
     ]
